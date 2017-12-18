79baebfa2a6c128a8016f23d37dcaee35b81bf64
louise.lapain01 <louise.lapain01@stclairconnect.ca>
2017-12-18 17:40:18 -0500

    motions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 $( document ).ready(function() {
     console.log( "ready!" );
   $('#btn').on('click', function(){
-   $('.movie-poster-one-bg').addClass('grow-left');
-  $('.movie-poster-two-bg').addClass('grow-right');
-  $('#vs').addClass('rotate');
-  });
+  $('.movie-poster-one').addClass('grow-left');
+  $('.movie-poster-two').addClass('grow-right');
+  $('#vs').addClass('fade-in');
+  
+  $('.movie-poster-one').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
+      console.log("left-finished");
+      });
+    $('.movie-poster-two').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
+      console.log("right-finished");
+      });
+     $('#vs').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
+      console.log("vs-finished");
+      });
+  
+  }); 
 });
